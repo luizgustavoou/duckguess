8ad71e44311e5f61a94c128c121a07665be37179
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-03-01 21:18:28 -0300

    feature(appbutton): melhorar estilo css

--- duckguess-ui/src/App.tsx
+++ duckguess-ui/src/App.tsx
@@ -1,27 +1,24 @@
-import './App.css'
-import AppButton from './components/form/AppButton'
-import char from '../public/duck-sing-removebg-preview.png'
+import "./App.css";
+import AppButton from "./components/form/AppButton";
+import logo from "./assets/duck-sing-removebg-preview.png";
 
 export default function App() {
-
   return (
-    <div className='page'>
-      <div className='section'>
-        <h1>
-          DuckGuess
-        </h1>
+    <div className="container">
+      <div className="section">
+        <h1>DuckGuess</h1>
       </div>
 
-      <div className='second section'>
-        <img alt='PatoMarketero' src={char} />
+      <div className="second section">
+        <img alt="PatoMarketero" src={logo} />
       </div>
 
-      <div className='section format-flex'>
+      <div className="section format-flex">
         <div>
-          <AppButton content='Jogar' />
+          <AppButton content="Jogar" />
         </div>
-        <AppButton content='Regras' />
+        <AppButton content="Regras" />
       </div>
     </div>
-  )
+  );
 }
