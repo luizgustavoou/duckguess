da3513f61f51542c5ff6161d81227a21da324844
Joab Urbano <joab.uaraujo@gmail.com>
2024-03-04 20:13:17 -0300

    Alterações no game core

--- duckguess-ui/src/views/game-core/GameCore.tsx
+++ duckguess-ui/src/views/game-core/GameCore.tsx
@@ -1,16 +1,26 @@
 import { useAppSelector } from "../../hooks/useAppSelector";
 import { selectGame } from "../../slices/game-slice";
+import Game from "../game/Game";
 import "./GameCore.css";
+import { SlActionRedo } from "react-icons/sl";
 
-function GameCore() {
-  const { guess } = useAppSelector(selectGame);
+export default function GameCore() {
+  const { guess, playerOne, playerTwo } = useAppSelector(selectGame);
 
   console.log(guess);
   return (
-    <>
-      <h1>GameCore page!</h1>
-    </>
+    <Game>
+      <>
+        <p className="player">
+          <li>{playerOne.name}</li>
+          <li>{playerOne.score}</li>
+        </p>
+        <p className="player">
+          <li>{playerTwo.name}</li>
+          <li>{playerTwo.score}</li>
+        </p>
+        <p className="link"><SlActionRedo /> Pular a vez</p>
+      </>
+    </Game>
   );
 }
-
-export default GameCore;
