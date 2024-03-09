32fb87141a2c5a0af4b2278d4def8b2ee5393491
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-03-09 14:30:35 -0300

    [FRONTEND] feature(GameWrongAnswer): destacar resposta

--- duckguess-ui/src/views/game-wrong-answer/GameWrongAnswer.tsx
+++ duckguess-ui/src/views/game-wrong-answer/GameWrongAnswer.tsx
@@ -1,6 +1,6 @@
-import "./GameWrongAnswer.css"
+import "./GameWrongAnswer.css";
 import Game from "../game/Game";
-import patoDecepcionado from "../../assets/duck-disappointed.png"
+import patoDecepcionado from "../../assets/duck-disappointed.png";
 import AppButton from "../../components/form/AppButton";
 import { useNavigate } from "react-router-dom";
 import { RoutesPath } from "../../utils/routes-path";
@@ -9,32 +9,30 @@ import { resetGuess, selectGame } from "../../slices/game-slice";
 import { useAppDispatch } from "../../hooks/useAppDispatch";
 
 export default function GameWrongAnswer() {
+  const navgate = useNavigate();
+  const { guess } = useAppSelector(selectGame);
+  const dispatch = useAppDispatch();
 
-    const navgate = useNavigate();
-    const {guess} = useAppSelector(selectGame);
-    const dispatch = useAppDispatch();
-
-    return(
-        <>
-            <Game>
-                <div className="body">
-                    <h1>Que pena, ninguém acertou!</h1>
-                    <h2>A resposta correta era {guess?.answer}</h2>
-                    <img
-                        className="img"
-                        alt="PatoDecepcionado"
-                        src={patoDecepcionado}
-                    />
-                    <AppButton
-                        content={"Tela de perguntas"}
-                        type={"submit"}
-                        onClick={(e) => {
-                            navgate(RoutesPath.GAME_CHOOSE);
-                            dispatch(resetGuess());
-                        }}
-                    />
-                </div>
-            </Game>
-        </>
-    )
+  return (
+    <>
+      <Game>
+        <div className="game-wrong-answer">
+          <h1>Que pena, ninguém acertou!</h1>
+          <h2>
+            A resposta correta era{" "}
+            <span className="answer">{guess?.answer}</span>
+          </h2>
+          <img className="img" alt="PatoDecepcionado" src={patoDecepcionado} />
+          <AppButton
+            content={"Tela de perguntas"}
+            type={"submit"}
+            onClick={(e) => {
+              navgate(RoutesPath.GAME_CHOOSE);
+              dispatch(resetGuess());
+            }}
+          />
+        </div>
+      </Game>
+    </>
+  );
 }
