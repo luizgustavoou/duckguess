8745f4923ba6077d87b09d7289aed6725bf59e10
Joab Urbano <joab.uaraujo@gmail.com>
2024-03-18 20:41:35 -0300

    Formatação da tela de dicas

--- duckguess-ui/src/views/rules/Rules.tsx
+++ duckguess-ui/src/views/rules/Rules.tsx
@@ -4,6 +4,7 @@ import { RoutesPath } from "../../utils/routes-path";
 import Game from "../game/Game";
 import "./Rules.css";
 import { MouseEvent } from "react";
+import DuckRuler from "../../assets/duck-ruler.webp"
 
 function Rules() {
   const navigate = useAppNavigate();
@@ -18,11 +19,12 @@ function Rules() {
     <Game>
       <div className="rules">
         <p>
-          Inspirado no popular programa da TV, no jogo das três pistas você
-          precisa descobrir a palavra secreta usando até 3 pistas como dica.
-          Quanto menos pistas você revelar para descobrir a palavra, mais pontos
-          irá marcar.
+          Inspirado no popular programa da TV, no <span>jogo das três pistas </span>
+          você precisa descobrir a palavra secreta usando até <span>três pistas </span>
+          como dica.Quanto menos pistas você revelar para descobrir a palavra, <span>
+          mais pontos irá marcar</span>.
         </p>
+        <img className="img" alt="duck-ruler" src={DuckRuler} />
         <AppButton type="button" content="Voltar" onClick={handleClick} />
       </div>
     </Game>
