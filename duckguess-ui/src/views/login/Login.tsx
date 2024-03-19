b661bd4ddaea7baf67496dca68b473562f374241
Joab Urbano <joab.uaraujo@gmail.com>
2024-03-19 19:55:32 -0300

    Tela RegisterGuess

--- duckguess-ui/src/views/login/Login.tsx
+++ duckguess-ui/src/views/login/Login.tsx
@@ -3,8 +3,19 @@ import { AppInput } from "../../components/form/AppInput";
 import Game from "../game/Game";
 import "./Login.css"
 import DuckLogin from "../../assets/duck-login.png"
+import { useAppNavigate } from "../../hooks/useAppNavigate";
+import { RoutesPath } from "../../utils/routes-path";
 
 export default function Login() {
+
+    const navigate = useAppNavigate();
+
+    const handleSubmit = (
+        e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>
+    ) => {
+        navigate(RoutesPath.REGISTER_GUESS);
+    }
+
     return(
 
         <Game>
@@ -21,6 +32,7 @@ export default function Login() {
                 <AppButton
                     content={"Login"}
                     type={"button"}
+                    onClick={handleSubmit}
                 />
             </div>
         </Game>
--- duckguess-ui/src/views/register-guess/RegisterGuess.tsx
+++ duckguess-ui/src/views/register-guess/RegisterGuess.tsx
@@ -1,7 +1,39 @@
+import AppButton from "../../components/form/AppButton";
+import { AppInput } from "../../components/form/AppInput";
+import Game from "../game/Game";
+import "./RegisterGuess.css"
+
 export default function RegisterGuess() {
     return(
-        <div>
-            RegisterGuess
-        </div>
+        <Game>
+            <div className="register-guess">
+                <div className="">
+                    <AppInput
+                        type="text"
+                        placeholder="Theme"
+                    />
+                </div>
+                <AppInput
+                    type="text"
+                    placeholder="Guess"
+                />
+                <AppInput
+                    type="text"
+                    placeholder="Hint 01"
+                />
+                <AppInput
+                    type="text"
+                    placeholder="Hint 02"
+                />
+                <AppInput
+                    type="text"
+                    placeholder="Hint 03"
+                />
+                <AppButton
+                    content={"Register Guess"}
+                    type={"button"}
+                />
+            </div>
+        </Game>
     )
 }
