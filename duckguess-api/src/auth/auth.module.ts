8d096adf9faa1a35e82898df34fa0ba948f70adb
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-03-20 20:10:29 -0300

    [BACKEND] feature(LocalAuth): criar classe separada

--- duckguess-api/src/auth/auth.module.ts
+++ duckguess-api/src/auth/auth.module.ts
@@ -3,15 +3,20 @@ import { AuthService } from './service/auth.service';
 import { AuthServiceImpl } from './service/impl/auth.impl.service';
 import { UserModule } from 'src/user/user.module';
 import { LocalStrategy } from './local-strategy/local-strategy';
+import { PassportModule } from '@nestjs/passport';
+import { AuthController } from './auth.controller';
+import { UserController } from 'src/user/user.controller';
 
 @Module({
-  imports: [UserModule], 
+  imports: [UserModule, PassportModule],
   providers: [
     {
       provide: AuthService,
       useClass: AuthServiceImpl,
     },
+    UserController,
     LocalStrategy,
   ],
+  controllers: [AuthController],
 })
 export class AuthModule {}
