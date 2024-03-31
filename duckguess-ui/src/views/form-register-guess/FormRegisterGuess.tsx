68cf6c05100b9a6dd1ed087f19605dff70dff09e
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-03-30 23:21:54 -0300

    refatorar

--- duckguess-ui/src/views/form-register-guess/FormRegisterGuess.tsx
+++ duckguess-ui/src/views/form-register-guess/FormRegisterGuess.tsx
@@ -1,36 +1,29 @@
 import AppButton from "../../components/form/AppButton";
 import { AppInput } from "../../components/form/AppInput";
-import Game from "../game/Game";
-import DuckWriter from "../../assets/duck-writer.png"
-import "./FormRegisterGuess.css"
+import DuckWriter from "../../assets/duck-writer.png";
+import "./FormRegisterGuess.css";
 import AppSelect from "../../components/form/AppSelect";
+import { HiOutlineX } from "react-icons/hi";
 
-export default function FormRegisterGuess() {
-    return(
-        <div className="form-register-guess">
-            <img alt="duck-writer" src={DuckWriter} />
-            <AppSelect content={['opção 01', 'opção 02', 'opção 03', 'opção 04']} />
-            <AppInput
-                type="text"
-                placeholder="Guess"
-            />
-            <AppInput
-                type="text"
-                placeholder="Hint 01"
-            />
-            <AppInput
-                type="text"
-                placeholder="Hint 02"
-            />
-            <AppInput
-                type="text"
-                placeholder="Hint 03"
-            />
-            <AppButton
-                content={"Register Guess"}
-                type={"button"}
-            />
-        </div>
+export interface IFormRegisterGuess {
+  handleClose: () => void;
+}
 
-    )
+export default function FormRegisterGuess({ handleClose }: IFormRegisterGuess) {
+  return (
+    <div className="form-register-guess">
+      <header>
+        <button onClick={handleClose}>
+          <HiOutlineX size={30} />
+        </button>
+      </header>
+      <img alt="duck-writer" src={DuckWriter} />
+      <AppSelect content={["opção 01", "opção 02", "opção 03", "opção 04"]} />
+      <AppInput type="text" placeholder="Guess" />
+      <AppInput type="text" placeholder="Hint 01" />
+      <AppInput type="text" placeholder="Hint 02" />
+      <AppInput type="text" placeholder="Hint 03" />
+      <AppButton content={"Register Guess"} type={"button"} />
+    </div>
+  );
 }
--- duckguess-ui/src/views/register-guess/RegisterGuess.tsx
+++ duckguess-ui/src/views/register-guess/RegisterGuess.tsx
@@ -1,102 +1,104 @@
 import Game from "../game/Game";
-import "./RegisterGuess.css"
+import "./RegisterGuess.css";
 import CardGuessRegistered from "../card-guess-registered/CardGuessRegistered";
 import { useState } from "react";
 import FormRegisterGuess from "../form-register-guess/FormRegisterGuess";
 import { HiOutlineX } from "react-icons/hi";
 import { HiPlus } from "react-icons/hi";
+import AppButton from "../../components/form/AppButton";
 
 export default function RegisterGuess() {
-    const guessMoc = [
+  const guessMoc = [
+    {
+      id: "ac632dd8-a53d-4cd1-b58c-b032dc7aa097",
+      answer: "JavaScript",
+      hints: [
         {
-            id: "ac632dd8-a53d-4cd1-b58c-b032dc7aa097",
-            answer: "JavaScript",
-            hints: [
-              {
-                id: "1102f96a-332c-4156-ab3d-13cc497d16f8",
-                text: "Utiliza o prototype",
-              },
-              {
-                id: "7097349a-6920-4c8d-8ff0-16ba418b9a62",
-                text: "Não é tipada",
-              },
-              {
-                id: "d01b8088-c1e1-4027-9762-12c4bc59120e",
-                text: "Uma das linguagens mais famosas",
-              },
-            ],
-            opened: true,
-          },
-          {
-            id: "c3fc8df0-8fbf-43ad-87b2-b638d1482b55",
-            answer: "Python",
-            hints: [
-              {
-                id: "4cf0cb85-0f38-4ab6-a400-9a040731d2e8",
-                text: "Linguagem de programação de alto nível",
-              },
-              {
-                id: "e8e9b2b6-c4a1-4811-8fa0-48d4b239bb61",
-                text: "Amplamente utilizada em desenvolvimento web e científico",
-              },
-              {
-                id: "839c7fc1-09ef-4d92-bf4c-914cc5d0fd33",
-                text: "Conhecida por sua simplicidade e legibilidade",
-              },
-            ],
-            opened: true,
-          },
-          {
-            id: "b4054e37-4980-4d8b-8177-4bbf3fa89c02",
-            answer: "Java",
-            hints: [
-              {
-                id: "2f0465c4-c3d4-46d6-bef0-1b45a03fbd61",
-                text: "Linguagem orientada a objetos",
-              },
-              {
-                id: "0c3055c9-26b1-4a33-a2e1-1950d5d91625",
-                text: "Amplamente utilizada no desenvolvimento de aplicativos móveis e empresariais",
-              },
-              {
-                id: "1a514e57-832b-42f1-99d7-38d3835f49c2",
-                text: "Possui uma máquina virtual que permite a portabilidade do código",
-              },
-            ],
-            opened: true,
-          }
-    ]
+          id: "1102f96a-332c-4156-ab3d-13cc497d16f8",
+          text: "Utiliza o prototype",
+        },
+        {
+          id: "7097349a-6920-4c8d-8ff0-16ba418b9a62",
+          text: "Não é tipada",
+        },
+        {
+          id: "d01b8088-c1e1-4027-9762-12c4bc59120e",
+          text: "Uma das linguagens mais famosas",
+        },
+      ],
+      opened: true,
+    },
+    {
+      id: "c3fc8df0-8fbf-43ad-87b2-b638d1482b55",
+      answer: "Python",
+      hints: [
+        {
+          id: "4cf0cb85-0f38-4ab6-a400-9a040731d2e8",
+          text: "Linguagem de programação de alto nível",
+        },
+        {
+          id: "e8e9b2b6-c4a1-4811-8fa0-48d4b239bb61",
+          text: "Amplamente utilizada em desenvolvimento web e científico",
+        },
+        {
+          id: "839c7fc1-09ef-4d92-bf4c-914cc5d0fd33",
+          text: "Conhecida por sua simplicidade e legibilidade",
+        },
+      ],
+      opened: true,
+    },
+    {
+      id: "b4054e37-4980-4d8b-8177-4bbf3fa89c02",
+      answer: "Java",
+      hints: [
+        {
+          id: "2f0465c4-c3d4-46d6-bef0-1b45a03fbd61",
+          text: "Linguagem orientada a objetos",
+        },
+        {
+          id: "0c3055c9-26b1-4a33-a2e1-1950d5d91625",
+          text: "Amplamente utilizada no desenvolvimento de aplicativos móveis e empresariais",
+        },
+        {
+          id: "1a514e57-832b-42f1-99d7-38d3835f49c2",
+          text: "Possui uma máquina virtual que permite a portabilidade do código",
+        },
+      ],
+      opened: true,
+    },
+  ];
 
-    const [modalOpen, setModalOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
 
-    const controlModal = () => {
-        setModalOpen(!modalOpen);
-    }
+  const controlModal = () => {
+    setModalOpen(!modalOpen);
+  };
 
-    return(
-        <Game>
-            <div className="register-guess">
-              <button onClick={controlModal}><HiPlus />Adicionar Guess</button>
-              <div className="register-guess-body">
-                  {guessMoc && (guessMoc.map((guess) => {
-                      return(
-                          <>
-                              <CardGuessRegistered guess={guess} />
-                          </>
-                      )
-                  }))}
-              </div>
-              {modalOpen && (
-                <div className="register-guess-modal-overlay">
-                  <div className="exit">
-                    <button onClick={controlModal}>
-                      <HiOutlineX size={30} />
-                    </button>
-                  </div>
-                  <FormRegisterGuess />
-                </div>
-              )}
-            </div>
-        </Game>
-    )
+  return (
+    <Game>
+      <div className="register-guess">
+        <AppButton
+          content={"Adicionar Adivinhação"}
+          type="button"
+          onClick={controlModal}
+        ></AppButton>
+
+        <div className="register-guess-body">
+          {guessMoc &&
+            guessMoc.map((guess) => {
+              return (
+                <>
+                  <CardGuessRegistered guess={guess} />
+                </>
+              );
+            })}
+        </div>
+        {modalOpen && (
+          <div className="register-guess-modal-overlay">
+            <FormRegisterGuess handleClose={controlModal} />
+          </div>
+        )}
+      </div>
+    </Game>
+  );
 }
